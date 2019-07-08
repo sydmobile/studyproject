f213a62430b3094f885c867fbb642ec137ab49e3
sunyidong <sunyidong516@163.com>
2019-07-08 18:40:01 +0800

    RecyclerView demo

--- app/src/main/java/com/syd/study/recyclerview/ListViewActivity.java
+++ app/src/main/java/com/syd/study/recyclerview/ListViewActivity.java
@@ -1,29 +1,26 @@
 package com.syd.study.recyclerview;
 
 import android.content.Context;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.ShapeDrawable;
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.syd.study.R;
 import com.syd.study.base.BaseActivity;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
+import androidx.annotation.Nullable;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
@@ -35,10 +32,14 @@ import butterknife.ButterKnife;
  * @author syd
  * @version 1.0
  */
-public  class ListViewActivity extends BaseActivity {
+public class ListViewActivity extends BaseActivity {
 
     @BindView(R.id.lv)
     ListView lv;
+    private List<String> mDatas;
+    @BindView(R.id.tv_create_menu)
+    TextView tvCreateMenu;
+
     // https://www.jianshu.com/p/4e8e4fd13cf7
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -48,24 +49,57 @@ public  class ListViewActivity extends BaseActivity {
         init();
     }
 
+
+
+    /**
+     * 每次都会触发
+     * @param menu     就是我们的上下文菜单（可以通过 xml 也可以通过 代码实现）
+     * @param v        要注册 menu 的 View
+     * @param menuInfo 含有 menu 的信息，如果注册的 View  是 ListView 的话 menuInfo 则是 AdapterContextMenuInfo
+     *                 否则为 null
+     */
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        getMenuInflater().inflate(R.menu.menu,menu);
+//        AdapterView.AdapterContextMenuInfo adapterContextMenuInfo = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        Log.e("onCreateContextMenu",  "log");
+        getMenuInflater().inflate(R.menu.menu, menu);
     }
 
+    /**
+     * 选中后触发
+     * @param item 选中的 menu item
+     * @return 没有用
+     */
     @Override
     public boolean onContextItemSelected(MenuItem item) {
 
+        switch (item.getItemId()) {
+            case R.id.menu1:
+                Toast.makeText(this, item.getTitle() + "增加", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menu2:
+                Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
         return super.onContextItemSelected(item);
     }
 
-    public void init(){
+    public void init() {
+
+
         List<String> list = new ArrayList<>();
-        for (int i= 0 ;i<30;i++){
-            list.add("string"+i);
+        for (int i = 0; i < 30; i++) {
+            list.add("string" + i);
         }
-        registerForContextMenu(lv);
+        //region 制造数据
+
+        // 制造数据
+        mDatas = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            mDatas.add("数据" + i);
+        }
+        //endregion
+
+        registerForContextMenu(tvCreateMenu);
         // 创建 ArrayAdapter 对象
 //        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,list);
 //        AdapterCommon<String> adapterCommon = new AdapterCommon<>(list, this, new AdapterCommon.CallBack() {
@@ -81,7 +115,8 @@ public  class ListViewActivity extends BaseActivity {
 //        String [] name = new String[]{"wwww","aaa","wwwww","rrrr"};
 //        String [] address = new String[]{"北京","上海","广东","深圳"};
 //        int [] test = new int[]{22,33,44,55};
-//        int [] picture = new int[]{R.drawable.button,R.mipmap.ic_launcher_round,R.mipmap.ic_launcher,R.mipmap.ic_launcher_round};
+//        int [] picture = new int[]{R.drawable.button,R.mipmap.ic_launcher_round,R.mipmap.ic_launcher,R.mipmap
+//        .ic_launcher_round};
 //        // 定义一个 HashMap 构成的列表以
 //        ArrayList<HashMap<String,Object>> listItem = new ArrayList<>();
 //        for (int i = 0;i<name.length;i++){
@@ -92,11 +127,12 @@ public  class ListViewActivity extends BaseActivity {
 //            hashMap.put("picture",picture[i]);
 //            listItem.add(hashMap);
 //        }
-//        SimpleAdapter mSimpleAdapter = new SimpleAdapter(this,listItem,R.layout.item_simple_adapter,new String[]{"name","address","test","picture"},new int[]{R.id.tv_one,R.id.tv_two,R.id.tv_three,R.id.tv_four});
+//        SimpleAdapter mSimpleAdapter = new SimpleAdapter(this,listItem,R.layout.item_simple_adapter,new
+//        String[]{"name","address","test","picture"},new int[]{R.id.tv_one,R.id.tv_two,R.id.tv_three,R.id.tv_four});
 //        lv.setAdapter(mSimpleAdapter);
         // 需要布局和数据一一对应，在创建适配器的填入参数的时候比较繁琐
 
-        lv.setAdapter(new MyAdapter(this,list));
+        lv.setAdapter(new MyAdapter(this, list));
 
         // 页面可以看到几个 item ，第一次就执行 getView多少次。当再出现一个 item 的时候再调用一次 getView
         // contentView 的个数为页面可见的个数 +1 。然后不可见的 item 放入回收栈中，等待下一次回收使用。
@@ -104,13 +140,14 @@ public  class ListViewActivity extends BaseActivity {
 
     }
 
-    class MyAdapter extends BaseAdapter{
+    class MyAdapter extends BaseAdapter {
         // 用来填充布局
         private LayoutInflater mInflater;
         private Context context;
         private List<String> listItem;
+
         // 构造方法
-        public MyAdapter(Context context,List<String> listItem){
+        public MyAdapter(Context context, List<String> listItem) {
             this.context = context;
             this.listItem = listItem;
             this.mInflater = LayoutInflater.from(context);
@@ -133,10 +170,10 @@ public  class ListViewActivity extends BaseActivity {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null){
-                convertView = mInflater.inflate(R.layout.item_rlv,parent,false);
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.item_rlv, parent, false);
             }
-            Log.e("getView","convertView:"+convertView.toString());
+            Log.e("getView", "convertView:" + convertView.toString());
             return convertView;
         }
     }
